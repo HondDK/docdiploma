f6af7121079186153bcbbfe484fc3a53ebcfba97
Danil Konovalenko <dkonovalenko@wooppay.kz>
2024-12-26 16:46:01 +0500

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,26 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   alias: {
     "@": '../src'
   },
+
   dir: {
     pages: './src/app/routes'
+  },
+
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './src/shared/ui'
   }
 })
